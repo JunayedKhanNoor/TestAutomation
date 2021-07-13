b66567a7de90be5b161fdcee9506dc81bd952a65
JunayedKhanNoor <junayednoor@gmail.com>
2021-07-13 16:04:24 +0600

    Screenshot

---
+++ src/test/java/com/Screenshot/SpecificScreenshot.java
@@ -0,0 +1,26 @@
+package com.Screenshot;
+
+import com.Base.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SpecificScreenshot extends TestBase {
+    public static void main(String[] args) throws IOException {
+        chromeLaunch();
+        openTestURL("https://demo.opencart.com/index.php?route=account/login");
+        takeScreenshot("Element");
+    }
+    public static void takeScreenshot(String name) throws IOException {
+        WebElement captureElement = driver.findElement(By.cssSelector("#content > div > div:nth-child(2) > div"));
+        //take screenshot
+        File srcFile = ((TakesScreenshot)captureElement).getScreenshotAs(OutputType.FILE);
+        //save image
+        FileUtils.copyFile(srcFile,new File("./src/test/Screenshots/"+name+".png"),true);
+    }
+}
